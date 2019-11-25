e17aa2b79737333bc6a63ff76fc3e07f8e912d6a
Viktor Bengtsson <vibe16@student.bth.se>
2019-11-25 17:28:39 +0100

    Updated test.sh - Retry connecting to MySQL

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 source venv/bin/activate
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo db migration failed, retrying in 5 secs...
+    sleep 5
+done
 pylint --rcfile=.pylintrc app tests
 python -m coverage run --rcfile=.coveragerc -m py.test -c pytest.ini tests/unit
 python -m coverage run --rcfile=.coveragerc -m py.test -c pytest.ini tests/integration
