195692bf3f5c3da16ef9c75e65589bd1a68708d9
Viktor Bengtsson <vibe16@student.bth.se>
2019-12-11 13:38:17 +0100

    Updated Ansible. Added .grafana with all dashboards

--- ansible/roles/database/files/setup.sql
+++ ansible/roles/database/files/setup.sql
@@ -1,5 +1,6 @@
-CREATE USER 'exporter' @'localhost' IDENTIFIED BY '12345' WITH MAX_USER_CONNECTIONS 3;
+CREATE USER 'exporter'@'%' IDENTIFIED BY '12345' WITH MAX_USER_CONNECTIONS 3;
 GRANT PROCESS,
 REPLICATION CLIENT,
 SELECT
-  ON *.* TO 'exporter' @'localhost';
+  ON *.* TO 'exporter'@'%';
+GRANT SELECT ON performance_schema.* TO 'exporter'@'%';
